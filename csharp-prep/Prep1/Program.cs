45d50c69db1ce5e3807ce9294794630b1735f3c0
v-develops <victorcaputo1001@gmail.com>
2024-01-13 14:46:54 -0300

    Prep 1 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Line space //
+        Console.WriteLine($"");
+
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        // Line space //
+        Console.WriteLine($"");
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
+        // Line space //
+        Console.WriteLine($"");
     }
 }
