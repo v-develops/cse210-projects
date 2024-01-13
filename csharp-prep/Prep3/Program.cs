9bd474cb4385aec52c69be98667ae6605cd92c50
v-develops <victorcaputo1001@gmail.com>
2024-01-13 20:19:12 -0300

    prep 3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int answer = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != answer)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (answer > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (answer < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
